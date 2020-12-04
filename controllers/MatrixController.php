62c23d139f89c506ca1657a0adfeab0ffedbffa6
abdelghani <abdelghaniechlahi514@gmail.com>
2020-12-04 04:26:25 -0100

    diagonolize MatrixA

--- controllers/MatrixController.php
+++ controllers/MatrixController.php
@@ -1,5 +1,119 @@
 <?php 
 class MatrixController 
 {
-    
-}
+   public function solve($operation, $matrixA, $matrixB)
+   {
+
+
+     //    get the operations
+        switch($operation){
+          
+            case 'multiplication':
+                $this->multiply($matrixA, $matrixB);
+            break;
+
+        }
+
+
+   }
+
+  
+
+   protected function multiply($matrixA, $matrixB)
+   {
+
+        // render an error if multiplication cannot be done
+     //    if (!$this->canMuliply()){
+     //        return 'error';
+     //    }
+
+          // diapgonolize the matriceA if it is 2d 
+
+         
+          echo 'the origin is ';
+          print_r($matrixA);
+         
+          //////////////////  transfrom matrix A into 1D
+
+          if (is_array($matrixA[0])){
+               
+
+               $i = 0;
+               $j = 0;
+              
+               $matrixA1D = [];
+
+             foreach ($matrixA as $rows) {
+                  
+               foreach ($rows as $element){
+
+                    array_push($matrixA1D, $element);
+
+               }
+             }
+          
+             // transform 1D array into 2D
+             $matrixADiag = [];
+             for ($i=0; $i < count($matrixA) ; $i++) { 
+                  
+
+               $matrixADiag[$i] = [];
+               for ($j=$i; $j <count($matrixA1D) ; $j+=count($matrixA)) { 
+                    
+                     array_push($matrixADiag[$i], $matrixA1D[$j]);
+
+               }
+             }
+
+             echo "the diagonale of A is: ";
+             print_r($matrixADiag);
+
+
+          
+
+          //    for($i=0, $j=0; $i < count($matrixA1D); $i++, $j+=count()){
+               
+               // echo $matrixA1D[$j];
+               // echo 'hello world';
+
+          //    }
+          
+               
+
+             
+
+              
+               
+               // // convert matrixA into a string
+               // $matrixAElement = '';
+               //  foreach ($matrixA as $row){
+               //       foreach ($row as $e){
+
+               //           $matrixAElement .= (string) ' '. $e;
+               //       }
+               //  }
+
+               //  // convert the string into 1D array
+               //  $matrixAElement = explode(' ', $matrixAElement);
+
+               //  // remove the first item
+               //  $matrixAElement = array_slice($matrixAElement, 1, count($matrixAElement) - 1);
+
+
+          }// end coversion 
+
+          //
+
+
+
+
+          // 1 - get the dimentions of the net matrix
+          $cols = count($matrixB[0]);
+          $rows = count($matrixA);
+
+          // 2- 
+
+          
+     }// end multiplication function
+
+}// end class defintion
